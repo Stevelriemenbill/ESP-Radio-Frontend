806897ffb1e86ef1a8590787fcb2e087577730fc
Daniel Muench <d.muench@sap.com>
2022-03-09 09:01:11 +0100

    add radio logo

--- src/App.vue
+++ src/App.vue
@@ -7,22 +7,14 @@
     >
       <div class="d-flex align-center">
         <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
+          alt="ESPRadio Logo"
+          class="mr-2"
           contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
+          src="@/assets/radio_logo_2.jpg"
+          width="80"
         />
 
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
+        <v-toolbar-title>Radio</v-toolbar-title>
       </div>
     </v-app-bar>
 
